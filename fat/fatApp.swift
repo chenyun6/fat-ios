35f83682cfee3f22202c5573f701b00fa31e513d
zhangyuyang <yuyang.zhang@tongdun.net>
2026-01-12 22:44:30 +0800

    Initial Commit

---
+++ fat/fatApp.swift
@@ -0,0 +1,17 @@
+//
+//  fatApp.swift
+//  fat
+//
+//  Created by Hello World on 2026/1/12.
+//
+
+import SwiftUI
+
+@main
+struct fatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
